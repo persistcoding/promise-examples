88c11115c3db1f9dcdda4b0a34fbb4cceebed654
minnaq <persist.coding@gmail.com>
2017-02-16 14:34:42 +0800

    update to correct

--- source/node/test.js
+++ source/node/test.js
@@ -5,9 +5,9 @@ var startCallback = app.startCallback
 
 describe("callback", function(){
   describe("id:0",function(){
-    it("should show 'candy: 是未成年女性1'", function(done) {
+    it("should show 'candy: 是未成年女性'", function(done) {
       startCallback(0,function(str){
-        str.should.equal('candy: 是未成年女性1');
+        str.should.equal('candy: 是未成年女性');
         done()
       })
     })
